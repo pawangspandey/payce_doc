9b345f7ae6a4f6d3826b48bb4223bf4e447e7077
pawan pandey <pawang.s.pandey@gmail.com>
2016-09-25 14:58:41 +0530

    dax

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-09-25T06:10:02.389Z",
+    "time": "2016-09-25T09:27:06.200Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
